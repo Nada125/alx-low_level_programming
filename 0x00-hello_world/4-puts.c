a6661acdc3ef8cbc167b55e446bbea9f3c8f22c6
Nada <nadaalihassan34@gmail.com>
2023-02-16 15:59:19 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  *main - main block
  *return : 0
  */
-int main (void)
+int main()
 {
-  puts ("\"Programming is like building a multilingual puzzle");
-    return (0) ;
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
 
