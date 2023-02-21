89be2d424194dcebd73f28cb6dd3478a68c92064
Nada <nadaalihassan34@gmail.com>
2023-02-21 15:42:26 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
-#include "main.h";
+#include "main.h"
 
 /**
  * print_alphabet - a function that prints the alphabet, in lowercase
  *
  * Return: Always 0 (Succes)
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int n;
 
